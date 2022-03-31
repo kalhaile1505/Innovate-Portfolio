704e9d2ac94362c1279d306034066556a24d7e16
Kalkidan Haile <kalhaile1505@gmail.com>
2022-03-31 10:45:53 +0100

    Updating portfolio with java script and scss

---
+++ front-end/jquery_intro/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#change-colour").click(function(){
+        $("h1").toggleClass("red-title");
+        });
+    $("#hide-text").click(function(){
+        $("p").toggle();
+        });
+        $("#activate-dark-mode").click(function(){
+            $("body").toggleClass("dark-mode");
+        });    
+
+});
